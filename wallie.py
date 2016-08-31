4b8c39e9b91e5f6626698b5856f4fcf7926915b5
melevir <melevir@gmail.com>
2016-08-31 08:26:02 +0300

    добавлен скрипт про Валли

---
+++ wallie.py
@@ -0,0 +1,20 @@
+import random
+
+
+def get_wallie_action():
+    possible_actions = [
+        'считает новых пользователей',
+        'готовит новую задачу',
+        'что-то проверяет',
+        'придумывает новую ачивку',
+        'проверяет чью-то задачу',
+        'проставляет ссылки в энциклопедии',
+        'вычитывает статью',
+        'ищет стажировки для студентов',
+    ]
+    return 'Валли ' + random.choice(possible_actions)
+
+
+if __name__ == '__main__':
+    action = get_wallie_action()
+    print action
